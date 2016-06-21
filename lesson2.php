5908c21b08173cd98d993e9d66bd100a9f2a3ff6
Андрей <andronicum@yandex.ru>
2016-06-21 13:47:11 +0300

    Приветствие

---
+++ lesson2.php
@@ -0,0 +1,36 @@
+﻿
+<?php /* Домашнее заданик №2 - Выбираем приветствие в зависимости от
+времени суток (день, утро , вечер, ночь)*/
+	echo "Сейчас " . date("G") . " ч. " . date("i") . " мин." ; 
+?>
+	<br>
+	<br> 
+<?php
+	$friend = " Друг"; /**/
+	$time=date("G");
+	
+	switch($time){
+		
+		
+		case $time>=6 && $time<12:
+			echo "Доброе утро!" . $friend . " !!!";
+		break;
+		
+		
+		case $time>=12 && $time<18:
+			echo "Добрый день!"  . $friend . " !!!";
+		break;
+		
+		
+		case $time>=18 && $time<24:
+			echo "Добрый вечер!" . $friend . " !!!";
+		break;
+		
+		
+		case $time>=0 && $time<6:
+			echo "Доброй ночи!" . $friend . " !!!";
+		break;
+		
+	}
+?>
+
